bcc279eb3fe05245733a4227ec2fae19b1e27f06
Dillon Bly <dillonbly@gmail.com>
2013-01-13 11:41:26 -0800

    Switch to HTML5 style urls (no hashtag) and enable channel persistence.
    Add client_data to expose white and black cards to client.

--- app/js/game.js
+++ app/js/game.js
@@ -2,10 +2,15 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('game', ['game.filters', 'game.services', 'game.directives']).
-  config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/game', {templateUrl: 'html/game.html', controller: GameController});
+angular.module(
+  'game',
+  ['game.filters', 'game.services', 'game.directives'],
+  function($routeProvider, $locationProvider) {
+    $routeProvider.when('/game', {templateUrl: 'partials/game.html', controller: GameController});
     $routeProvider.when('/hand', {templateUrl: 'partials/hand.html', controller: HandController});
     $routeProvider.when('/judge', {templateUrl: 'partials/judge.html', controller: JudgeController});
     $routeProvider.otherwise({redirectTo: '/'});
-  }]);
+
+    // No need for hashtag in URLs.
+    $locationProvider.html5Mode(true);
+  });
